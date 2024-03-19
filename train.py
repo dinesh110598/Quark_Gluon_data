6ed8dc0d39e717c397bded5261dd6776aed84ce9
Dinesh <dineshramakrishnan2012@gmail.com>
2024-03-19 08:41:50 -0500

    Added crossentropy loss and plots for different datasets

--- train.py
+++ train.py
@@ -27,19 +27,25 @@ def loss_fn(net, X, A, mask, periodic=True):
     else:
         X_xhit = X[:, :, 0]
     
-    mse_xhit = torch.nn.MSELoss()(X_xhit, Y[:, :, 0])
-    mse_yhit = torch.nn.MSELoss()(X[:, :, 1], Y[:, :, 1])
+    mse_xhit = nn.MSELoss()(X_xhit, Y[:, :, 0])
+    mse_yhit = nn.MSELoss()(X[:, :, 1], Y[:, :, 1])
     mse_hit = mse_xhit + mse_yhit
     
-    mse_ener = torch.nn.MSELoss()(X[:, :, 2], Y[:, :, 2])
-    mse_chan = torch.nn.MSELoss()(X[:, :, 3:], Y[:, :, 3:])
-    mse = mse_hit + mse_ener + mse_chan
+    mse_ener = nn.MSELoss()(X[:, :, 2], Y[:, :, 2])
+    # CE loss with masking 
+    mce_loss = nn.CrossEntropyLoss(reduction='none')
+    X_chan = X[:, :, 3:].view(-1, 3)
+    Y_chan = Y[:, :, 3:].view(-1, 3)
+    mce_chan = mce_loss(Y_chan, X_chan)
+    mce_chan = (mce_chan*mask.view(-1)).mean()
+    
+    mse = mse_hit + mse_ener + mce_chan
     KL_div = -0.5*torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
     graph_reg = L1 + L2
     
     # wt_reg = sum([p.abs().sum() for p in net.parameters()])
     
-    return mse + KL_div + 0.1*graph_reg, mse_hit, mse_ener, L2
+    return mse + KL_div + 0.1*graph_reg, mse_hit, mse_ener, mce_chan
 
 def train_loop(net: GraphVAE, epochs, batch_size=64, lr=1e-3, 
                 device=torch.device("cpu"), periodic=True):
@@ -53,20 +59,20 @@ def train_loop(net: GraphVAE, epochs, batch_size=64, lr=1e-3,
     hit_mse_list = []
     for ep in range(epochs):
         net = net.to(device)
-        ep_loss, ep_E_mse, ep_hit_mse, ep_L2, count = 0., 0., 0., 0., 0.
+        ep_loss, ep_E_mse, ep_hit_mse, ep_chan_mce, count = 0., 0., 0., 0., 0.
         for (X, NL, mask) in data_loader:
             X = X.to(device)
             mask = mask.to(device)
             A = eval_A(NL.to(device).int())*mask.unsqueeze(2)
             
             opt.zero_grad()
-            loss, hit_mse, E_mse, L2 = loss_fn(net, X, A, mask, periodic)
+            loss, hit_mse, E_mse, chan_mce = loss_fn(net, X, A, mask, periodic)
             loss.backward()
             
             ep_loss += float(loss.item())
             ep_E_mse += float(E_mse.item())
             ep_hit_mse += float(hit_mse.item())
-            ep_L2 += float(L2.item())
+            ep_chan_mce += float(chan_mce.item())
             count += 1
             
             opt.step()
@@ -78,12 +84,12 @@ def train_loop(net: GraphVAE, epochs, batch_size=64, lr=1e-3,
         ep_loss /= count
         ep_hit_mse /= count
         ep_E_mse /= count
-        ep_L2 /= count
+        ep_chan_mce /= count
         print("Epoch : {}".format(ep+1), 
               "Loss: {:.5f}".format(ep_loss),
               "E mse: {:.5f}".format(ep_E_mse), 
               "Hit mse: {:.5f}".format(ep_hit_mse),
-              "L2: {:.5f}".format(ep_L2))
+              "Channel mce: {:.5f}".format(ep_chan_mce))
         
         loss_list.append(ep_loss)
         E_mse_list.append(ep_E_mse)
